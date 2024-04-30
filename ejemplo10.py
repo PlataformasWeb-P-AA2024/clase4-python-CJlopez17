738338bc06ae83b034e17b376f3ec4a4e1dd21fe
Camilo López <camilojoloco123@icloud.com>
2024-04-30 09:46:35 -0500

    tarea termianda

--- ejemplo10.py
+++ ejemplo10.py
@@ -4,9 +4,28 @@ lineas = archivo.readlines()
 
 lineas = [l.split("|") for l in lineas]
 
-for x in lineas:
-	cadena = """<b> Torneo:</b> %s <br> <b> Ganador:</b> %s""" % (x[0], x[9])
-	print(cadena)
-	archivo_generado = open("data/%s.html" % (x[9]), "w")
-	archivo.generado.writelines("%s\n" % (cadena))
-	archivo.generado.close()
+for linea in lineas:
+    torneo = linea[0]
+    ganador = linea[9]
+
+    contenido_html = f"""
+        <!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <title>Detalles del Torneo</title>
+        </head>
+        <body>
+            <h1>Detalles del Torneo</h1>
+            <p><strong>Torneo:</strong> {torneo}</p>
+            <p><strong>Ganador:</strong> {ganador}</p>
+        </body>
+        </html>
+        """
+
+    nombre_archivo_html = f"data/{ganador.replace(' ', '_')}_en_{torneo.replace(' ', '_')}.html"
+
+    with open(nombre_archivo_html, 'w', encoding='utf-8') as archivo_html:
+        archivo_html.write(contenido_html)
+
+print("Creados con exito")
