12982da7911fe538bf1279bc9efca25070b2b055
Camilo López <camilojoloco123@icloud.com>
2024-04-29 20:55:27 -0500

    ejercicios en python

---
+++ ejemplo07.py
@@ -0,0 +1,11 @@
+diccionario = {}
+diccionario['nombre'] = "Camilo Jose"
+diccionario['apellido'] = "Lopez Correa"
+diccionario['ciudad'] = "Catacocha"
+
+print(diccionario.keys())
+print('------')
+print(diccionario['apellido']
+print('------')
+for x in diccionario.keys():
+	print(diccionario[x])
---
+++ ejemplo08.py
@@ -0,0 +1,2 @@
+diccionario02 = {'nombre': 'Camilo', 'apellido': 'Lopez', 'edad': '20'}
+diccionario02['sueldo'] = 450.06
